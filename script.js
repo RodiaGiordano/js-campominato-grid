d0fd1c7ae7c32fb60ac04ab912e70fd59b353604
RodiaGiordano <rodiagiordano93@gmail.com>
2023-08-21 15:40:14 +0200

    feat: JS script

---
+++ script.js
@@ -0,0 +1,47 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+
+const cellContainer = document.getElementById("wrapper");
+const buttonGrid = document.getElementById('button')
+
+const max = 100
+
+buttonGrid.addEventListener('click', function(){
+
+    cellContainer.innerHTML = "";
+    let cell;
+    
+    
+    for(let i = 1; i <= max; i++){
+        
+        cell = document.createElement('div');      
+        
+        cell.classList.add('box');
+        
+        cell.setAttribute('data-index', i);
+        
+        cellContainer.append(cell);
+
+        let cellShow = cell.getAttribute('data-index')
+        
+        cell.addEventListener('click', function(){
+            this.classList.add('bg-primary');
+            
+            this.innerText = cellShow;
+
+
+        })
+    }
+    
+   
+    
+    
+    
+
+
+})
+
+
